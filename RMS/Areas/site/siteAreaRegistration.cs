a5a022a8b58d74163817516e0c64ba4c3fd0c494
alexyomar <alexyomar@gmail.com>
2012-09-03 20:03:42 -0430

    1.0.2

---
+++ RMS/Areas/site/siteAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace RMS.Areas.site
+{
+    public class siteAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "site";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "site_default",
+                "site/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
