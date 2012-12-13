89655197189fa4851a0547f8a15bea767bb3b7da
alexyomar <alexyomar@gmail.com>
2012-12-12 20:11:16 -0430

    2.0.3

--- RMS/Controllers/HomeController.cs
+++ RMS/Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using RMS.Models;
 using System.Web.Security;
+using System.Net.Mail;
+using System.Text;
 
 namespace RMS.Controllers
 {
@@ -55,16 +57,74 @@ namespace RMS.Controllers
                                     string email,
                                     string empresa,
                                     string celular,
-                                    string arrive, 
+                                    string arrive,
                                     string departure,
-                                    string destino, 
-                                    string adultos, 
-                                    string ninos, 
+                                    string destino,
+                                    string adultos,
+                                    string ninos,
                                     string hotel,
                                     string pasajes,
                                     string[] servicios,
                                     string mensaje)
         {
+
+            MailMessage mailmessage;
+
+            mailmessage = new MailMessage(new MailAddress("promocionesrm@regionalmarketing.com.ve"), new MailAddress(System.Configuration.ConfigurationManager.AppSettings["email"]));
+            mailmessage.BodyEncoding = Encoding.Default;
+            mailmessage.Subject = "Solicitud de presupuesto Web";
+            mailmessage.Body = "<h1>Planilla de solicitud de presupuesto por Web</h1>";
+            mailmessage.Body += "<p>Datos de la planilla:</p><br /><br />";
+            mailmessage.Body += "<ul>";
+            mailmessage.Body += "<li>";
+            mailmessage.Body += "Nombre del contacto: " + nombre;
+            mailmessage.Body += "</li>";
+            mailmessage.Body += "<li>";
+            mailmessage.Body += "Email: " + email;
+            mailmessage.Body += "</li>";
+            mailmessage.Body += "<li>";
+            mailmessage.Body += "Empresa: " + empresa;
+            mailmessage.Body += "</li>";
+            mailmessage.Body += "<li>";
+            mailmessage.Body += "Celular: " + celular;
+            mailmessage.Body += "</li>";
+            mailmessage.Body += "<li>";
+            mailmessage.Body += "Fecha de llegada: " + arrive;
+            mailmessage.Body += "</li>";
+            mailmessage.Body += "<li>";
+            mailmessage.Body += "Fecha de salida: " + departure;
+            mailmessage.Body += "</li>";
+            mailmessage.Body += "<li>";
+            mailmessage.Body += "Adultos:" + adultos;
+            mailmessage.Body += "</li>";
+            mailmessage.Body += "<li>";
+            mailmessage.Body += "Niños:" + ninos;
+            mailmessage.Body += "</li>";
+            mailmessage.Body += "<li>";
+            mailmessage.Body += "Hotel: " + hotel;
+            mailmessage.Body += "</li>";
+            mailmessage.Body += "<li>";
+            mailmessage.Body += "Incluir Pasajes: " + pasajes;
+            mailmessage.Body += "</li>";
+            mailmessage.Body += "<li>";
+            mailmessage.Body += "Servicios a incluir: ";
+            foreach (var item in servicios)
+            {
+                mailmessage.Body += item + " ";
+            }
+            mailmessage.Body += "</li>";
+            mailmessage.Body += "<li>";
+            mailmessage.Body += "Observaciones: " + mensaje;
+            mailmessage.Body += "</li>";
+            mailmessage.Body += "</ul><br /><br />";
+            mailmessage.Body += "Planilla generada el " + DateTime.Now.ToString();
+            mailmessage.Priority = MailPriority.High;
+            mailmessage.IsBodyHtml = true;
+
+            SmtpClient smtpMail = new SmtpClient();
+
+            smtpMail.Send(mailmessage);
+
             return View("ContactOk");
         }
 
