b7c1f60c84218a8938cb0af5922d83431db81f87
alexyomar <isturiz>
2013-01-20 22:29:48 -0430

    2.5
    Website Layout merged.

---
+++ RMS/Areas/Web/Controllers/VEController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RMS.Areas.Web.Controllers
+{
+    public class VEController : Controller
+    {
+        //
+        // GET: /Web/VE/
+
+        public RMS.Models.RegionalEntities __db = new Models.RegionalEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Seccion(int Id, string name)
+        {
+            IEnumerable<RMS.Models.Content> __data = __db.Content.Where(u => u.IdContentType.Equals(Id));
+            return View(__data);
+        }
+
+        public ActionResult PartialContenido(int Id)
+        {
+            RMS.Models.Content __data = __db.Content.Where(u => u.Id.Equals(Id)).Single();
+            return View(__data);
+        }
+
+
+    }
+}
---
+++ RMS/Areas/Web/webAreaRegistration.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+
+namespace RMS.Areas.web
+{
+    public class webAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Web";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "web_secciones",
+                "Web/{controller}/{action}/{id}/{name}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+            context.MapRoute(
+                "web_default",
+                "Web/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
