354c94d1243718a1ca2ebc912c62b112a1ff861a
alexyomar <isturiz>
2013-01-24 23:09:19 -0430

    2.6

--- RMS/Areas/Web/Controllers/VEController.cs
+++ RMS/Areas/Web/Controllers/VEController.cs
@@ -18,12 +18,15 @@ namespace RMS.Areas.Web.Controllers
         {
             ViewBag.Banners = __db.Banner.Where(u => u.IdBannerType.Equals(1));
             ViewBag.Destacado = __db.Banner.Where(u => u.IdBannerType.Equals(2)).OrderByDescending(u => u.Timestamp).FirstOrDefault();
+            ViewBag.Content = __db.Content.Where(u => u.Active.Equals(true) && u.Featured.Equals(true)).OrderByDescending(u => u.TimestampEdit);
 
             return View();
         }
         public ActionResult Seccion(int Id, string name)
         {
             IEnumerable<RMS.Models.Content> __data = __db.Content.Where(u => u.IdContentType.Equals(Id));
+
+            ViewBag.Title = __db.ContentType.Where(u => u.Id.Equals(Id)).First().Name + " - Regional Marketing";
             return View(__data);
         }
         public ActionResult Presupuesto()
